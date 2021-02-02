07004f83059355155b1cfcdf04036ff673611912
yuezihao <yuezihao@troila.com>
2021-02-02 13:50:59 +0800

    update add react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+/*
+ * @Author: yzh
+ * @Date: 2021-02-02 10:18:59
+ * @LastEditTime: 2021-02-02 13:50:02
+ * @LastEditors: yzh
+ * @Description: 
+ * @FilePath: /main-react/src/App.js
+ */
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return (
+    <div className="App">
+      home
+    </div>
+  )
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+// export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
+/*
+ * @Author: yzh
+ * @Date: 2021-02-02 10:18:59
+ * @LastEditTime: 2021-02-02 13:46:53
+ * @LastEditors: yzh
+ * @Description: 
+ * @FilePath: /main-react/src/index.js
+ */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import { registerMicroApps, start } from 'qiankun';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App />,
   </React.StrictMode>,
   document.getElementById('root')
 );
 
+// registerMicroApps([
+//   {
+//     name: 'vueApp',
+//     entry: '//localhost:8080',
+//     container: '#container',
+//     activeRule: '/app-vue',
+//   }
+// ]);
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals();  // console.log
